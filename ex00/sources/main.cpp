55ba12782556cde03f2290d57e11c992d27ec8cd
Pierrick NEAU <pineau@made-f0Br8s7.clusters.42paris.fr>
2024-01-18 15:45:23 +0100

    update zombie 1

--- ex00/sources/main.cpp
+++ ex00/sources/main.cpp
@@ -6,16 +6,14 @@
 /*   By: pineau <pineau@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 13:59:42 by pineau            #+#    #+#             */
-/*   Updated: 2024/01/18 15:11:49 by pineau           ###   ########.fr       */
+/*   Updated: 2024/01/18 15:44:08 by pineau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-int	main(int argc, char **argv) {
-	if (argc != 2)
-		return (0);
-	Zombie zombie(argv[1]);
+int	main(void) {
+	Zombie zombie("Zombie1");
 	zombie.announce();
 	randomChump("Zombie2");
 	Zombie	*zombie2 = newZombie("Zombie3");
